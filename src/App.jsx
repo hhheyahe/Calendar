5eec8558efe3755f9c890572c4dffbf3da6efe17
Me <katerina251003@gmail.com>
2024-07-02 14:00:05 +0300

    refactor: delete commented lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import dayjs from 'dayjs';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faCalendarDays } from '@fortawesome/free-solid-svg-icons';
 
 import Calendar from './components/Calendar';
+// import Modal from './components/modal/modal';
 
 import './index.css'
 
@@ -14,7 +14,7 @@ library.add(faCalendarDays);
 class App extends React.Component {
     state = {
         date: null,
-        modalActive: true
+        modalActive: false
         // isOpen: false
     };
 
@@ -35,15 +35,16 @@ class App extends React.Component {
                 // {date && <p>Выбранная дата: {date.toLocaleDateString()}</p>}
             // <div>
             //     {date && <p>Выбранная дата: {dayjs(date).format('YYYY-MM-DD')}</p>}
-            <div>
+            <div className='app'>
                 <FontAwesomeIcon
                     className="calendarIcon"
                     icon={['fas', 'calendar-days']}
                     onClick={this.toggleModal}
                 />
                 <Calendar 
-                    onChange={(selectedDates) => console.log(selectedDates)}
+                    // onChange={(selectedDates) => console.log(selectedDates)}
                     // isOpen={this.state.isOpen}
+                    onChange={this.handleDateChange}
                     active={this.state.modalActive}
                     setActive={this.toggleModal}
                 />
