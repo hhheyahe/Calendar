3bea1c080987f7e48b41766332631c6348a2ec1e
Me <katerina251003@gmail.com>
2024-07-03 18:10:44 +0300

    fix: correct getMonthData()

--- src/components/Calendar/calendar.js
+++ src/components/Calendar/calendar.js
@@ -1,84 +1,50 @@
 import dayjs from "dayjs";
+import "dayjs/locale/be";
+import weekday from 'dayjs/plugin/weekday';
+
+dayjs.extend(weekday);
 
 const DAYS_IN_WEEK = 7;
 
-const DAYS_IN_MONTH = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-
-const Month = {
-    January: 0,
-    February: 1,
-    March: 2,
-    April: 3,
-    May: 4,
-    June: 5,
-    July: 6,
-    August: 7,
-    September: 8,
-    October: 9,
-    Novermber: 10,
-    December: 11
-};
-
 export function areEqual(a, b) {
-    if (!a || !b) return false;
+  if (!a || !b) return false;
 
-    return (
-        // a.getFullYear() === b.getFullYear() &&
-        // a.getMonth() === b.getMonth() &&
-        // a.getDate() === b.getDate()
-        a.year() === b.year() &&
-        a.month() === b.month() &&
-        a.date() === b.date()
-    );
+  return (
+    a.year() === b.year() &&
+    a.month() === b.month() &&
+    a.date() === b.date()
+  );
 }
 
 export function isLeapYear(year) {
-    return !((year % 4) || (!(year % 100) && (year % 400)));
+  return !((year % 4) || (!(year % 100) && (year % 400)));
 }
 
 export function getDayOfWeek(date) {
-    // const dayOfWeek = date.getDay();
-    const dayOfWeek = date.day();
-
-    if (dayOfWeek === 0) return 6;
-
-    return dayOfWeek - 1;
-}
-
-export function getDaysInMonth(date) {
-    // const month = date.getMonth();
-    // const year = date.getFullYear();
-    const month = date.month();
-    const year = date.year();
-    const daysInMonth = DAYS_IN_MONTH[month];
-    
-    if (isLeapYear(year) && month === Month.February) {
-        return daysInMonth + 1;
-    } else {
-        return daysInMonth;
-    }
+  return date.weekday();
 }
 
 export function getMonthData(year, month) {
-    const result = [];
-    // const date = new Date(year, month);
-    const date = dayjs().year(year).month(month);
-    const daysInMonth = getDaysInMonth(date);
-    const monthStartsOn = getDayOfWeek(date);
-    let day = 1;
+  const result = [];
+  const date = dayjs().locale('be').year(year).month(month);
+  const daysInMonth = date.daysInMonth();
+  const monthStartsOn = getDayOfWeek(date.date(1));
+  let day = 1;
 
-    for (let i = 0; i < (daysInMonth + monthStartsOn / DAYS_IN_WEEK); i++) {
-        result[i] = [];
-
-        for (let j = 0; j < DAYS_IN_WEEK; j++) {
-            if ((i === 0 && j < monthStartsOn) || day > daysInMonth) {
-                result[i][j] = undefined;
-            } else {
-                // result[i][j] = new Date(year, month, day++);
-                result[i][j] = dayjs().year(year).month(month).date(day++);
-            }
-        }
+  for (let i = 0; day <= daysInMonth; i++) {
+    result[i] = [];
+    for (let j = 0; j < DAYS_IN_WEEK; j++) {
+      if ((i === 0 && j < monthStartsOn) || day > daysInMonth) {
+        result[i][j] = undefined;
+      } else {
+        result[i][j] = dayjs().locale('be').year(year).month(month).date(day++);
+      }
     }
+  }
 
-    return result;
+  if (result.length && result[result.length - 1].every(day => day === undefined)) {
+    result.pop();
+  }
+
+  return result;
 }
